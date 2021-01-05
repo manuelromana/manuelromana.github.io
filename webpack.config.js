239ebbffea5b958bdf75e69e52cf507008e1d686
manuelromana <mr.romanamanuel@gmail.com>
2021-01-05 14:57:13 -0500

    commit workflow

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[sc]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './',
+  },
+};
