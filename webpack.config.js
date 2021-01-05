fdf2b1143199a3769399211aa6882b18ae872e5b
RomanaManuel <mr.romanamanuel@gmail.com>
2021-01-06 00:42:29 +0100

    workflow webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    publicPath: 'dist/',
   },
   module: {
     rules: [
