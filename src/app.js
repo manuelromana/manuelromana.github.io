95f205573fd32de3b73774d945a7aab17229c72f
RomanaManuel <mr.romanamanuel@gmail.com>
2021-01-06 19:26:46 +0100

    add background

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import './scss/main.scss';
+
+const svgList = document.getElementById('svg-list');
+const svgTemplateCircle = document.getElementById('svg-circle');
+const circles = [];
+for (let i = 0; i < 5; i++) {
+  circles.push(document.importNode(svgTemplateCircle.content, true));
+}
+
+console.log(circles);
+circles.forEach(circleTemplate => {
+  svgList.append(circleTemplate);
+});
